53e40d4676595d55ed3bff0d5f3618d18f16c626
sahand-edc <sahand.edc@gmailcom>
2023-12-25 21:09:57 +0330

    fix : add function html and js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-function easy() {
-  num = sudoku.board_string_to_grid(sudoku.generate("easy"));
-}
-function medium() {
-  num = sudoku.board_string_to_grid(sudoku.generate("medium"));
-}
-function hard() {
-  num = sudoku.board_string_to_grid(sudoku.generate("hard"));
-}
-function veryHard() {
-  num = sudoku.board_string_to_grid(sudoku.generate("very-hard"));
-}
-function insane() {
-  num = sudoku.board_string_to_grid(sudoku.generate("insane"));
-}
-function inhuman() {
-  num = sudoku.board_string_to_grid(sudoku.generate("inhuman"));
-}
+
 
 let num = sudoku.board_string_to_grid(sudoku.generate("easy"));
 function createTable() {
@@ -26,9 +9,8 @@ function createTable() {
     let numindex = num[i];
     for (let j = 0; j < numindex.length; j++) {
       if (numindex[j] == ".") {
-        row.innerHTML += `<th><input type="text" maxlength="1">${(numindex[
-          j
-        ] = " ")}</input></th>`;
+        row.innerHTML += `<th><input type="text" maxlength="1">${(numindex[j] =
+          " ")}</input></th>`;
       } else {
         row.innerHTML += `<th><span>${numindex[j]}</span></th>`;
       }
@@ -39,35 +21,36 @@ createTable();
 function clearTable() {
   document.getElementById("table").innerHTML = "";
 }
-easyButton.addEventListener("click", () => {
+
+function easy(){
   clearTable();
-  easy();
+    num = sudoku.board_string_to_grid(sudoku.generate("easy"));
   createTable();
-});
-mediumButton.addEventListener("click", () => {
-  clearTable();
-  medium();
-  createTable();
-});
-hardButton.addEventListener("click", () => {
-  clearTable();
-  hard();
-  createTable();
-});
-veryHardButton.addEventListener("click", () => {
-  clearTable();
-  veryHard();
-  createTable();
-});
-insaneButton.addEventListener("click", () => {
-  clearTable();
-  insane();
-  createTable();
-});
-inhumanButton.addEventListener("click", () => {
-  clearTable();
-  inhuman(inhuman);
-  createTable();
-});
+ }
+  function medium(){
+    clearTable();
+    num = sudoku.board_string_to_grid(sudoku.generate("medium"));
+    createTable();
+  }
+  function hard(){
+    clearTable();
+    num = sudoku.board_string_to_grid(sudoku.generate("hard"));
+    createTable();
+  }
+  function VeryHard(){
+    clearTable();
+    num = sudoku.board_string_to_grid(sudoku.generate("very-hard"));
+    createTable();
+  }
+  function insane(){
+    clearTable();
+    num = sudoku.board_string_to_grid(sudoku.generate("insane"));
+    createTable();
+  }
+  function inhuman(){
+    clearTable();
+    num = sudoku.board_string_to_grid(sudoku.generate("inhuman"));
+    createTable();
+  }
 
 
